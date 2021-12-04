ebb8fcc867851b291f0da6c854e2b057e7ba64a2
Drew Rodrigues <androdri@microsoft.com>
2021-12-04 08:01:54 -0500

    Open new window if app still active on mac and close app if all apps closed

--- main.js
+++ main.js
@@ -11,4 +11,14 @@ const createWindow = () => {
   window.loadFile("main.html");
 };
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
