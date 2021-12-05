90c8de29e15f9110d837700322acaaa8e6a6e3a6
Drew Rodrigues <androdri@microsoft.com>
2021-12-05 17:16:39 -0500

    Pull up backdrop

--- client/views/dashboard.tsx
+++ client/views/dashboard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "./view";
 import senior from "../images/senior.jpg";
 import videochat from "../images/videochat.jpg";
