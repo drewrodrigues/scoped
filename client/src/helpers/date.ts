d207f52b7f5bc769f52e44888a489e633892fca5
Drew Rodrigues <androdri@microsoft.com>
2022-01-12 10:04:04 -0600

    Add typing to utils

--- client/src/helpers/date.ts
+++ client/src/helpers/date.ts
@@ -1,12 +1,12 @@
-export function yesterdaysDate() {
+export function yesterdaysDate(): Date {
   return nDaysFromNow(-1);
 }
 
-export function todaysDate() {
+export function todaysDate(): Date {
   return new Date();
 }
 
-export function nDaysFromNow(n: number) {
+export function nDaysFromNow(n: number): Date {
   const date = new Date();
   const yesterday = date.setDate(date.getDate() + n);
   return new Date(yesterday);
