69dbe1b93dc78b2b3d72c7cdbec65914872aa3f7
Drew Rodrigues <androdri@microsoft.com>
2022-01-19 07:26:05 -0600

    Update interface

--- client/src/views/view.tsx
+++ client/src/views/view.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
 interface ViewProps {
-  children: JSX.Element | JSX.Element[];
+  children: JSX.Element | JSX.Element[] | any[];
 }
 
 export function View({ children }: ViewProps) {
-  return <main className="view">{children}</main>;
+  return (
+    <main className="p-[40px] overflow-y-scroll w-full max-w-[1000px] mx-auto">
+      {children}
+    </main>
+  );
 }
