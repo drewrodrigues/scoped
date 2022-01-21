fe426787252546b6783707f768d0761f2bf18894
Drew Rodrigues <androdri@microsoft.com>
2022-01-20 21:59:02 -0600

    Add showDismissed setting

--- client/src/components/goals/goalsToday.tsx
+++ client/src/components/goals/goalsToday.tsx
@@ -1,20 +1,44 @@
-import React from "react";
-import { FaArrowRight } from "react-icons/fa";
+import React, { useState } from "react";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+import { createOrSaveModel } from "../../data/modelCrud";
 import { useGoalsInSelectedScope } from "../../store/goalSlice";
 import { Button } from "../shared/button";
 import { GoalToday } from "./goalToday";
+import { ISettings } from "../../data/modelTypes";
 
 interface GoalTodayProps {}
 
 export function GoalsToday({}: GoalTodayProps) {
   const goals = useGoalsInSelectedScope();
+  const [showDismissed, setShowDismissed] = useState(() => {
+    const savedShowDismissedSetting = localStorage.getItem("SHOW_DISMISSED");
+    console.log({ savedShowDismissedSetting });
+    return !!savedShowDismissedSetting;
+  });
+
+  function saveDismissedSetting() {
+    if (!showDismissed) {
+      localStorage.setItem("SHOW_DISMISSED", "true");
+    } else {
+      localStorage.removeItem("SHOW_DISMISSED");
+    }
+    setShowDismissed(!showDismissed);
+  }
 
   return (
     <main>
-      <h2 className="font-bold">Goals</h2>
+      <header className="flex justify-between mb-[20px] items-center">
+        <h2 className="font-bold">Goals</h2>
+        <Button
+          text={showDismissed ? "Hide Dismissed" : "Show Dismissed"}
+          onClick={saveDismissedSetting}
+        >
+          {showDismissed ? <FaEyeSlash /> : <FaEye />}
+        </Button>
+      </header>
       <ul>
         {goals.map((goal) => (
-          <GoalToday goal={goal} />
+          <GoalToday goal={goal} showDismissed={showDismissed} />
         ))}
       </ul>
     </main>
